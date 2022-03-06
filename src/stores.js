11d8a0f641db174589678e64ed9c99ff64f6a662
Helbindi <55160812+Helbindi@users.noreply.github.com>
2022-03-05 22:23:45 -0800

    Update stores.js

--- src/stores.js
+++ src/stores.js
@@ -22,6 +22,7 @@ import {
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
+
 // Initialize Firebase
 initializeApp(firebaseConfig);
 
